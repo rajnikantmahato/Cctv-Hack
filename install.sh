4d7914c62afb201281aa44efff29cc7a387d4710
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-09-14 22:43:29 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt update -y
+apt upgrade -y
+apt install unzip -y
+clear
+echo Contact to 9122592250 on WhatsApp to get password
+echo Contact to 9122592250 on WhatsApp to get password
+
+echo Contact to 9122592250 on WhatsApp to get password
+
+
+unzip Cctv Hack.zip
+
